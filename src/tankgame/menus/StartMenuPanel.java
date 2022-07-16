560a9c2fac7221d29b0fdc858ba0c7f10860e1f0
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2022-07-15 19:50:56 -0700

    changed some assets to be 48x48 to not mess up tile calculations and placements | added new title screen

--- src/tankgame/menus/StartMenuPanel.java
+++ src/tankgame/menus/StartMenuPanel.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Objects;
 
 public class StartMenuPanel extends JPanel {
     private BufferedImage menuBackground;
@@ -17,7 +18,10 @@ public class StartMenuPanel extends JPanel {
     public StartMenuPanel(Launcher lf) {
         this.lf = lf;
         try {
-            menuBackground = ImageIO.read(this.getClass().getClassLoader().getResource("menu/title.png"));
+            menuBackground = ImageIO.read(
+                    Objects.requireNonNull(this.getClass().getClassLoader().getResource("menu/title.png"),
+                            String.format("Could not find %s", "menu/title.png"))
+            );
         } catch (IOException e) {
             System.out.println("Error cant read menu background");
             e.printStackTrace();
@@ -28,7 +32,10 @@ public class StartMenuPanel extends JPanel {
 
         start = new JButton("Start");
         start.setFont(new Font("Courier New", Font.BOLD, 24));
-        start.setBounds(150, 300, 150, 50);
+        start.setBounds(175, 375, 150, 50);
+        start.setBackground(Color.black);
+        start.setForeground(Color.red);
+        start.setFocusPainted(false);
         start.addActionListener((actionEvent -> {
             this.lf.setFrame("game");
         }));
@@ -37,7 +44,10 @@ public class StartMenuPanel extends JPanel {
         exit = new JButton("Exit");
         exit.setSize(new Dimension(200, 100));
         exit.setFont(new Font("Courier New", Font.BOLD, 24));
-        exit.setBounds(150, 400, 150, 50);
+        exit.setBounds(175, 450, 150, 50);
+        exit.setBackground(Color.black);
+        exit.setForeground(Color.red);
+        exit.setFocusPainted(false);
         exit.addActionListener((actionEvent -> {
             this.lf.closeGame();
         }));
