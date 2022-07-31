baca6f8e3411eda45902e5a8eb7ad6c72c2e7d67
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2022-07-31 13:34:35 -0700

    reduce text size

--- src/tankgame/menus/CheatCodePanel.java
+++ src/tankgame/menus/CheatCodePanel.java
@@ -16,12 +16,12 @@ public class CheatCodePanel extends MenuPanel {
 
         String[] labels = {"CHEAT CODES", "RENDER HITBOXES", "ONE SHOT ONE KILL"};
         for(int i = 0; i < labels.length; i++) {
-            this.add(super.createLabel(labels[i], 22, STARTING_X, STARTING_Y + (HEIGHT * i), this.WIDTH, HEIGHT));
+            this.add(super.createLabel(labels[i], 20, STARTING_X, STARTING_Y + (HEIGHT * i), this.WIDTH, HEIGHT));
         }
 
         labels = new String[]{"KEY BINDS", "F1", "F2"};
         for(int i = 0; i < labels.length; i++) {
-            this.add(super.createLabel(labels[i], 22, STARTING_X + this.WIDTH, STARTING_Y + (HEIGHT * i), this.WIDTH, HEIGHT));
+            this.add(super.createLabel(labels[i], 20, STARTING_X + this.WIDTH, STARTING_Y + (HEIGHT * i), this.WIDTH, HEIGHT));
         }
 
         help = super.createButton("BACK", 24, 175, 450, 175, 40, "help");
